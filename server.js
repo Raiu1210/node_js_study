63cc37f0a2950b5b6d3d8e8ac650c13111ce9d7b
Raiu1210 <baio1484@gmail.com>
2019-07-17 00:59:29 +0900

    did 3rd

--- server.js
+++ server.js
@@ -1,26 +1,41 @@
 var http = require('http');
+var ejs  = require('ejs');
+var qs   = require('querystring');
 var fs     = require('fs');
 var config = require('./config');
 var server = http.createServer();
-var msg;
+
+var posts = [];
+
+function renderForm(posts, res) {
+	var data = ejs.render(template, {
+		posts: posts
+	});
+
+	res.writeHead(200, {'Content-Type' : 'text/html'});
+   	res.write(data);
+   	res.end();
+}
+
+var template = fs.readFileSync(__dirname + '/hello.ejs', 'utf-8');
+
 
 server.on('request', function(req, res) {
-	res.writeHead(200, {'Content-Type' : 'text/plain'});
-
-	switch (req.url) {
-		case '/about':
-			msg = 'Welcome about page';
-			break;
-		case '/company':
-			msg = 'welcome my company page';
-			break;
-		default:
-			msg = 'page not found';
-			break;
+	if (req.method === 'POST') {
+		req.data = "";
+		req.on("readable", function() {
+			req.data += req.read() || '';
+			console.log(req.data);
+		})
+		req.on("end", function() {
+			var query = qs.parse(req.data);
+			console.log(query);
+			posts.push(query.user_name);
+			renderForm(posts, res);
+		})
+	} else {
+		renderForm(posts, res);
 	}
-
-    res.write(msg);
-    res.end();
 });
 
 server.listen(config.port);
